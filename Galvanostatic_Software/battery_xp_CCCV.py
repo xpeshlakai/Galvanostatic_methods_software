310b5e877c67355dfc68eb3cdf7d488032703346
xpeshlakai <108281685+xpeshlakai@users.noreply.github.com>
2022-08-09 12:51:24 -0400

    Update battery_xp_CCCV.py

--- Galvanostatic_Software/battery_xp_CCCV.py
+++ Galvanostatic_Software/battery_xp_CCCV.py
@@ -9,7 +9,8 @@ import matplotlib.pyplot as plt
 
 def run_manual_test(pstat, volt, dt, t_stop, descurr): #list of choosen currents
     """
-    Run a voltammetric test in maunal/direct mode. 
+    Run a galvanostatic test in manual/direct mode through the
+    implementation of a feedback loop. Voltage will adjust to desired current.
 
     pstat      = potentiostat
     volt_func  = output voltage function
@@ -17,24 +18,16 @@ def run_manual_test(pstat, volt, dt, t_stop, descurr): #list of choosen currents
     t_stop     = time for each trial
     """
     time_list, volt_list, curr_list = [], [], []
-
     print('{0:1.4s}, {1:1.4s}, {2:1.4s}'.format("Time", "Voltage", "Current"))
-    
-  
     t = 0
     cnt = 0
     t_prev = 0
     t_start = time.time()
     scheduler = sched.scheduler(time.time, time.sleep)
-	
     
     while desvolt != round(volt,3):
-   
-    	#Get current readings
-    	curr = pstat.get_curr()
-       
-    	#adjust voltage to desired current
-    	volt = volt_adjustment(curr, descurr, volt)
+    	curr = pstat.get_curr()                     #Get current readings
+    	volt = volt_adjustment(curr, descurr, volt) #adjust voltage to desired current
         
     	#format the results
     	#print('{0:1.6f}, {1:1.6f}, {2:1.6f}'.format(t+(t_stop), volt, curr))
@@ -50,13 +43,10 @@ def run_manual_test(pstat, volt, dt, t_stop, descurr): #list of choosen currents
     	scheduler.run()
     	t = time.time() - t_start
     	cnt+=1
-
-    	#Setting voltage w/response from current
-    	pstat.set_volt(volt)
+    	pstat.set_volt(volt) #Setting voltage w/respect to current
 
     while descurr2 != round(curr,3):
     	pstat.set_volt(volt)
-
     	#format the results
     	#print('{0:1.6f}, {1:1.6f}, {2:1.6f}'.format(t+(t_stop), volt, curr))
     	print('{0:1.6f}, {1:1.6f}, {2:1.6f}'.format(t,volt,curr))
@@ -86,7 +76,6 @@ def volt_adjustment(curr, liscurr, volt):
     #Keep voltage constant unless it changes
     if curr == descurr:
         volt = volt
-
     #Decrease voltage
     elif curr > descurr:
         if abs(curr-descurr)<3:
@@ -95,7 +84,6 @@ def volt_adjustment(curr, liscurr, volt):
             volt = volt-0.00001
         else:
             volt = volt-0.0001
-                	
     #Increase voltage
     elif curr < descurr:
         if abs(curr-descurr)<3:
@@ -110,20 +98,15 @@ def volt_adjustment(curr, liscurr, volt):
 if __name__ == '__main__':
 
     # Run parameters
-    dt = .001                # Scan rate seconds    
-    t_total = 360.0         # Each Trial duration in sec
-
-    #For constant current reaching voltage
-    descurr = 500      # Chosen Applied Currents
-    desvolt = 4.2    # Desired Volt 
-
-    #For constant voltage reaching current
-    descurr2 = 0.001 # Desired Curr
+    dt = .001         # Scan rate seconds    
+    t_total = 360.0   # Each Trial duration in sec
+    descurr = 500     # Constant Current Value
+    desvolt = 4.2     # Reaching Voltage
+    descurr2 = 0.001  # Final Current value to end test
 
     # Create device object, set voltage/current ranges and run test
     pstat = Potentiostat('/COM3')
     pstat.set_all_elect_connected(True)
-
     pstat.set_volt_range('5V')
     pstat.set_curr_range('1000uA')
 
@@ -138,11 +121,11 @@ if __name__ == '__main__':
     pstat.set_all_elect_connected(False)    
 
     # Plot results voltage vs time
-    plt.figure("Chronopotentiometry")
+    plt.figure("CCCV")
     plt.subplot(211)
     plt.plot(t,volt)
     current= ("current: "+str(liscurr)+"uA")
-    plt.suptitle(t="Chronopotentiometry")
+    plt.suptitle(t="CCCV")
     plt.title(label=current,fontsize=10,color="black")
     plt.xlabel('time (s)')
     plt.ylabel('potential (V)')
